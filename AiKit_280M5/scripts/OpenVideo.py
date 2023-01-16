7be0ddc432ca5f38cede58b418f090aa8835376c
wangWking <842749351@qq.com>
2023-01-16 14:36:47 +0800

    add Aikit_280M5

---
+++ AiKit_280M5/scripts/OpenVideo.py
@@ -0,0 +1,18 @@
+import cv2
+import platform
+
+if platform.system() == "Windows":
+    cap_num = 1
+elif platform.system() == "Linux":
+    cap_num = 0
+
+cap  = cv2.VideoCapture(cap_num)
+cap.set(3,640)
+cap.set(4,480)
+
+while cv2.waitKey(1)<0:
+    ret, frame = cap.read()
+    if not ret:
+        break
+    cv2.imshow('', frame)
+    
---
+++ AiKit_280M5/scripts/test.py
@@ -0,0 +1,25 @@
+from pymycobot.mycobot import MyCobot
+import time
+from pymycobot import PI_BAUD, PI_PORT
+import serial
+import serial.tools.list_ports
+
+plist = [
+    str(x).split(" - ")[0].strip() for x in serial.tools.list_ports.comports()
+]
+
+# mc = MyCobot(PI_PORT, PI_BAUD)
+mc = MyCobot(plist[0], 115200)
+
+coords = [ 
+        [135.0, -65.5, 280.1, 178.99, 5.38, -179.9],
+        [136.1, -141.6, 243.9, 178.99, 5.38, -179.9]
+    ]
+
+angles = [0, 0, 0, 0, 0, 0]
+
+mc.send_coords(coords[0], 20, 1)
+time.sleep(3)
+mc.send_coords(coords[1], 20, 1)
+
+
